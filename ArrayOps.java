d5591d951da90e8efa28964d31db15965e1f7f23
Elad Napp <eladnapp@h-MacBook-Pro-sl-Elad.local>
2024-02-22 12:13:12 +0200

    first

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,101 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
+
+        //System.out.println(findMissingInt(new int[] {4, 0, 1, 2, 5}));
+
     }
     
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+
+        int num = 0;
+        int sum1 = 0;
+        for (int i = 0; i <= array.length; i++ ) {
+                sum1= sum1 + i;
+        }
+
+        int sum2 = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum2 = sum2 + array[i];
+        }
+
+        return (sum1 - sum2);
     }
 
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+        
+        int max = array[0];
+        int second_max = array[0];
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max){
+                second_max = max;
+                max = array[i];
+            }
+            if ((array[i] < max) && (array[i] > second_max)) {
+                second_max = array[i];
+            }
+        }
+        return second_max;
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+
+
+        for (int i = 0; i < array1.length; i++) {
+            boolean t = false;
+            for (int j = 0; j < array2.length; j++) {
+                if (array1[i] == array2[j]) {
+                    t = true;
+                    break;
+                }
+            } 
+            if (t == false) {
+                return false;
+            }
+        }
+
+    
+
+        for (int j = 0; j < array2.length; j++) {
+            boolean t = false;
+            for (int i = 0; i < array1.length; i++) {
+                if (array2[j] == array1[i]) {
+                    t = true;
+                }
+            } 
+            if (t == false) {
+                return false;
+            }
+        }
+        return true; 
     }
 
     public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
-    }
+        
+        if (array[0] == array[array.length - 1]) {
+            return false;
+        }
 
+        if (array[0] < array[array.length - 1]) {
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] <= array[i + 1]) {
+                } else {
+                    return false;
+                }
+            }    
+        }
+
+
+        if (array[0] > array[array.length - 1]) {
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] >= array[i + 1]) {
+                } else {
+                    return false;
+                }
+            }    
+        }
+        return true;   
+    }
 }
+
+
--- StringOps.java
+++ StringOps.java
@@ -22,21 +22,107 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        //String str = "Hellow World";
+        //String xxx = "MY nAMe is eLAD";
+        //System.out.println(capVowelsLowRest(str));
+        //System.out.println(camelCase(xxx));
+        //System.out.println(allIndexOf("Hellow world", 'l'));
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+
+        String new_string = "";
+		char current_char = 0;
+		for(int i = 0; i < string.length(); i++) {
+			current_char = string.charAt(i);
+			if(current_char == ' ') {
+			    new_string += current_char;
+			}
+		    if(current_char >= 'a' && current_char <= 'z') { // if lower
+				if(current_char == 'a' || current_char == 'e'
+						|| current_char == 'i' || current_char == 'o' || 
+								current_char =='u') {
+					new_string += (char)(current_char - 32); // make upper
+				} else { // if not capital letter
+					new_string += current_char;
+				}
+			}
+			if (current_char >= 'A' && current_char <= 'Z') {{ // if upper cased
+				// if capital letter
+				if(current_char == 'A' || current_char == 'E'
+						|| current_char == 'I' || current_char == 'O' || 
+								current_char =='U') {
+					new_string += current_char; 
+				} else { // if not a capital letter, should be lower cased
+					new_string += (char)(current_char + 32);
+					}
+				}
+			}
+		}
+		return new_string;
     }
 
     public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+        String new_string = "";
+		char current_char = 0;
+		char last_char = 0;
+
+        for (int i = 0; i < string.length(); i++) {
+            current_char = string.charAt(i);
+            if (i == 0) {
+                if (current_char >= 'A' && current_char <= 'Z') {
+                    new_string += (char) (current_char + 32);
+                } else { 
+                    new_string += (char) (current_char);
+                }
+            }
+        
+
+            if (i != 0) {
+                
+                last_char = string.charAt(i - 1);
+
+                if (current_char >= 'A' && current_char <= 'Z') {
+                    new_string += (char) (current_char + 32);
+                } else {
+                if (last_char == ' ' && current_char != ' ') {
+                    if (current_char >= 'a' && current_char <= 'z' ) {
+                        new_string += (char) (current_char - 32);
+                    } else {
+                        new_string += (char) (current_char);
+                    }
+                } else {
+                if (current_char >= 'a' && current_char <= 'z') {
+                    new_string += (char) (current_char);
+                }
+                }
+                }
+            }
+        }
+        return new_string;
     }
 
+
+
     public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+        
+        int arrPush = 0;
+        int count = 0;
+
+        for (int i = 0; i < string.length(); i++){
+            if (string.charAt(i) == chr ){
+                count++;
+            }
+        }
+
+        int[] arr = new int [count];
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == chr) {
+                arr[arrPush] = i;
+                arrPush++;
+            }
+        }
+
+        return arr;
     }
 }
